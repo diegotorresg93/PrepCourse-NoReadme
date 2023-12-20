b4e86d48d3574bbfb64689707e82d8b9fd2ec9be
diegotorresg93 <diegotorresg93@gmail.com>
2023-12-19 22:25:28 -0500

    Math en JS

---
+++ Javascript/math.js
@@ -0,0 +1,23 @@
+Math.pow(2, 3);
+//8
+
+Math.round(0.49);
+//0
+
+Math.round(0.50);
+//1
+
+Math.floor(5.93);
+//5
+
+Math.ceil(3.27);
+//4
+
+Math.max(1, 2, 3, 4, 5);
+//5
+
+Math.min(1, 2, 3, 4, 5);
+//1
+
+Math.random();
+//0.8051478163546508
